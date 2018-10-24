96672db6a99876c4fedbd329c88646d78262d73a
L. K <LK00100100@gmail.com>
2018-10-23 22:19:46 -0400

    Enabled cookies

--- client/js/io/cookies.js
+++ client/js/io/cookies.js
@@ -89,7 +89,7 @@ var cookies = {
         d.setTime(d.getTime() + (numDays*24*60*60*1000));
 
         var expires = "expires="+ d.toUTCString();
-        //document.cookie = expires;
+        document.cookie = expires;
 
         /**
          * settings
